1f97626e79ba33604109b14e5f45eff78e5144e1
Arnav-Jhajharia <jhajhariaarnav@gmail.com>
2022-03-24 21:05:36 +0530

    nav-title-1

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Navbar from './Components/Navbar.js'
+import Titlebar from './Components/Titlebar.js'
+import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 function App() {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1100px)' })
+  console.log(isTabletOrMobile)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <section>
+       <Navbar />
+       {isTabletOrMobile && <Titlebar />}
+     </section>
   );
 }
 
---
+++ client/src/Components/Titlebar.js
@@ -0,0 +1,19 @@
+import './../Style/Titlebar.css'
+import { Component } from 'react'
+
+class Titlebar extends Component {
+    render() {
+        return (
+      <div className = "nav-bar">
+            <div className = "middle-aligned">
+                <p className = 'nav-home'><img className = 'svg-adjust rest-adjust' src = {process.env.PUBLIC_URL + '/images/home.svg'} /></p>
+            <p className = 'nav-deposit'><img className = 'nav-deposit-img svg-adjust' src = {process.env.PUBLIC_URL + '/images/plus.svg'} /></p>
+                <p className = 'nav-investments'><img className = 'svg-adjust rest-adjust' src = {process.env.PUBLIC_URL + '/images/stonks.svg'} /></p>
+                
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Titlebar
