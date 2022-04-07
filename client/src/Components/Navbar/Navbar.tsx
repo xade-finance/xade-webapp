b0b004ff7b92c790c827fc9662c8c8a35d27436f
Owini <owiniose@gmail.com>
2022-04-07 12:21:11 +0100

    commit for nav bar

--- client/src/Components/Navbar/Navbar.tsx
+++ client/src/Components/Navbar/Navbar.tsx
@@ -4,23 +4,25 @@ import { React, Link } from './../../module-manager'
 class Navbar extends React.Component {
     render() {
         return (
-        <div className = "title-bar">
-            <div className = "left-aligned">
-                <p className = 'logo'>Xade</p>
-                <p className = 'click-home' onClick = {() => {console.log('hi')}}>
-                    Home
-                </p>
-                <p className = 'click-investments'>Investments</p>
-                <p className = 'click-deposit'>Deposit / Withdraw</p>
-                <p className = 'click-offers'>Offers</p>
+            <div className = "title-bar">
+                <div className = "left-aligned">
+                    <p className = 'logo'>Xade</p>
+                    <a className = 'click-home' href="/">
+                        Home
+                    </a>
+                    <a className = 'click-investments' href="/investments">Investments</a>
+                    <a className = 'click-deposit' href="/Deposit">Deposit / Withdraw</a>
+                    <a className = 'click-offers' href="/Offers">Offers</a>
+    
+                    
+                </div>
+                <div className  = "right-aligned">
+                    <button className = 'click-settings'><img className = 'settings-logo svg-adjust' src = {process.env.PUBLIC_URL + '/images/settings.svg'} /></button>
+                
+                </div>
             </div>
-            <div className  = "right-aligned">
-                <button className = 'click-settings'><img className = 'settings-logo svg-adjust' src = {process.env.PUBLIC_URL + '/images/settings.svg'} /></button>
-            
-            </div>
-        </div>
-        )
-    }
+            )
+}
 }
 
 export default Navbar
