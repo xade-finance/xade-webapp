d59852f59b16f86e9b6839c6c251bae38afed45a
Arav <56185632+arav06@users.noreply.github.com>
2022-04-09 19:05:03 +0530

    Update web3auth.tsx

--- web3auth/src/services/web3auth.tsx
+++ web3auth/src/services/web3auth.tsx
@@ -162,7 +162,7 @@ request.send(json);
       done = true;
 }
 
-// window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
   };
 
 
