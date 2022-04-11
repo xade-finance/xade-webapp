4b603cbd11e360c49b60829883afb4018221699c
Arav <56185632+arav06@users.noreply.github.com>
2022-04-11 11:13:46 +0530

    prolly fixed the bug

--- web3auth/src/components/Main.tsx
+++ web3auth/src/components/Main.tsx
@@ -14,9 +14,7 @@ const Main = () => {
     login(WALLET_ADAPTERS.OPENLOGIN, "email_passwordless", email);
   }
 
-  const loggedInView = () => {
-    getUserInfo()
- }
+const loggedInView = () => (getUserInfo())
 
 //     <>
 //     <script>
